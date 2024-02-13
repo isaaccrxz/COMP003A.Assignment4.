d788a21a0e7a178553ae67987fd925873db432bb
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-02-12 22:33:04 -0800

    Add project files.

---
+++ COMP003A.Assignment4/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author: Isaac Cruz
+ * Course: COMP003A
+ * Purpose: looping statements
+ */
+namespace COMP003A.Assignment4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("for loop selection");
+            Console.WriteLine("Output a right triangle based on user input");
+            Console.WriteLine("".PadRight(50, '*'));
+
+            Console.WriteLine("Enter a positive whole number: ");
+            string playerResponse = Console.ReadLine();
+            
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            string output = "#";
+
+            for (int i = 0; i <= limit; i ++)
+            {
+                output += "#";
+            }
+
+           
+
+
+        }
+    }
+}
